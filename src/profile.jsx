da5226e733fda3de0747b6c2e1c7f98c5664a5c6
Kimiya Ahmadzadeh <kimiya.seysan@gmail.com>
2026-06-19 18:51:28 +0430

    added profile page

--- src/profile.jsx
+++ src/profile.jsx
@@ -1,5 +1,29 @@
+import "../styles/profile.css";
+import { TextField } from "@mui/material";
+import { Header } from "./header";
+
 export function Profile() {
+    const user = {
+        firstName: "USer1", lastName: "UUUSer1", bio: "Bio for test",
+        userName: "Username1", password: "pass1"
+    }
     return (
-        <div>My Profile</div>
+        <div className="profile">
+            <Header />
+            <div className="profile-card">
+                <div className="profile-pic">PICTURE</div>
+                <div className="profile-info">
+                    <div className="personal-info">
+                        <TextField variant="outlined" label="First Name" defaultValue={user.firstName} />
+                        <TextField variant="outlined" label="Last Name" defaultValue={user.lastName} />
+                        <TextField variant="outlined" label="Bio" defaultValue={user.bio} />
+                    </div>
+                    <div className="login-info">
+                        <TextField variant="outlined" label="User Name" defaultValue={user.userName} />
+                        <TextField variant="outlined" label="Password" defaultValue={user.password} type="password" />
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
