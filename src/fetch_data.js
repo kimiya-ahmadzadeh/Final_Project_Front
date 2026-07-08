c59057d912d508449e721a01a297106733af8864
Kimiya Ahmadzadeh <kimiya.seysan@gmail.com>
2026-07-08 18:21:15 +0430

    added dotenv files

--- src/fetch_data.js
+++ src/fetch_data.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = import.meta.env.VITE_BACKEND_BASE_URL;
 
 export function GetUserID() {
     const user = JSON.parse(localStorage.getItem("userAuth"));
